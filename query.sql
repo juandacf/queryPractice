a13ae3be80d8e9ba2d5a822fde5457dd27f53cc4
Juan David Caballero Fuentes <168665640+juandacf@users.noreply.github.com>
2025-03-01 16:04:11 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -23,7 +23,6 @@ FROM tab_productos a;
 
 SELECT a.fecha_movimiento, a.cantidad
 FROM tab_kardexs AS a
-WHERE a.tipo_movimiento = 'Entrada'
 GROUP BY(a.fecha_movimiento, a.cantidad)
 HAVING a.fecha_movimiento BETWEEN '2023-01-10' AND '2023-03-10'
 ORDER BY 1;
@@ -31,8 +30,23 @@ ORDER BY 1;
 
 SELECT a.fecha_movimiento, a.cantidad
 FROM tab_kardexs AS a
-WHERE a.tipo_movimiento = 'Entrada' AND
-a.fecha_movimiento BETWEEN '2023-01-10' AND '2023-03-10'
+WHERE a.fecha_movimiento BETWEEN '2023-01-10' AND '2023-03-10'
 GROUP BY(a.fecha_movimiento, a.cantidad)
 ORDER BY 1;
 --query para sumar todas las entradas de dinero por fecha en un rango específico sin usar having
+
+DROP VIEW IF EXISTS view_mov_dia;
+
+CREATE VIEW view_mov_dia AS
+	SELECT  a.fecha_movimiento,SUM(a.cantidad), a.tipo_movimiento
+	FROM tab_kardexs AS a
+	GROUP BY(a.fecha_movimiento, a.cantidad, a.tipo_movimiento)
+	HAVING a.fecha_movimiento BETWEEN '2023-01-10' AND '2023-03-10'
+	ORDER BY 1;
+
+	
+SELECT * FROM view_mov_dia;
+
+SELECT MAX(id_kardex) FROM tab_kardexs;
+
+--ejemplo para creación de vista. 
