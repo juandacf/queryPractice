99a9ff07510e4315ed1141b2937b1e7ff9fa495c
Juan David Caballero Fuentes <168665640+juandacf@users.noreply.github.com>
2025-03-08 14:26:26 -0500

    Update scriptAlex.sql

--- scriptAlex.sql
+++ scriptAlex.sql
@@ -8,8 +8,23 @@ DROP TABLE IF EXISTS tab_subcategorias_3;
 DROP TABLE IF EXISTS tab_subcategorias_2;
 DROP TABLE IF EXISTS tab_categorias;
 DROP TABLE IF EXISTS tab_unidades_medida;
-
+DROP TABLE IF EXISTS tab_pmtros;
 -- Crear tablas
+
+CREATE OR REPLACE FUNCTION fun_insert_pmtros(wid_empresa tab_pmtros.id_empresa%TYPE,
+											 wnom_empresa  tab_pmtros.nom_empresa%TYPE,
+											 wval_ivarefu tab_pmtros.val_ivarefu%TYPE,
+											 wval_iva_car tab_pmtros.val_iva_car%TYPE,
+											 wval_iva_otro tab_pmtros.val_iva_otro%TYPE,
+											 wval_descuento tab_pmtros.val_descuento%TYPE,
+											 wval_puntos tab_pmtros.val_puntos%TYPE) RETURNS VARCHAR AS 
+
+$$
+	BEGIN
+		INSERT INTO tab_pmtros VALUES (wid_empresa,wnom_empresa,wval_ivarefu, wval_iva_car, wval_iva_otro, wval_descuento,wval_puntos );
+	END;
+$$
+LANGUAGE PLPGSQL;
 
 CREATE TABLE IF NOT EXISTS tab_pmtros(
     id_empresa DECIMAL(10,0) NOT NULL,
